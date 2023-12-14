531337e40c66974b6878b62468ae47d0e00e959b
SamNew22 <153843173+SamNew22@users.noreply.github.com>
2023-12-14 22:48:32 +0400

    Update main.java

--- main.java
+++ main.java
@@ -1,4 +1,38 @@
+import java.util.Scanner;
 
 public class main {
+ public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Введите сообщение: ");
+        String message = scanner.nextLine();
+
+        System.out.println("Введите ключ: ");
+        String key = scanner.nextLine();
+
+        System.out.println("Зашифрованное сообщение: " + encrypt(message, key));
+    }
+
+    private static String encrypt(String message, String key) {
+        StringBuilder encryptedMessage = new StringBuilder();
+
+        for (int i = 0, j = 0; i < message.length(); i++) {
+            char messageChar = message.charAt(i);
+            char keyChar = key.charAt(j);
+
+            if (Character.isUpperCase(messageChar)) {
+                int encryptedChar = ((messageChar - 'А') + (keyChar - 'А')) % 32 + 'А';
+                encryptedMessage.append((char) encryptedChar);
+            } else if (Character.isLowerCase(messageChar)) {
+                int encryptedChar = ((messageChar - 'а') + (keyChar - 'а')) % 32 + 'а';
+                encryptedMessage.append((char) encryptedChar);
+            } else {
+                encryptedMessage.append(messageChar);
+            }
+
+            j = (j + 1) % key.length();
+        }
+
+        return encryptedMessage.toString();
+    }
 }
